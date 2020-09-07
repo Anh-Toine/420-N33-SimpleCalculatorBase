f7d733475367e6e1b47fe449d67ce4a4984aa313
Antoine Nwyn <Nick1234567890a@gmail.com>
2020-09-07 12:18:51 -0400

    Added 5 bitwise operators

--- Calculator/CalculatorEngine.cs
+++ Calculator/CalculatorEngine.cs
@@ -38,18 +38,64 @@ namespace SimpleCalculator
                     strbuild.Append(String.Format("The value {0} divided the value {1} is equal to {2}", Math.Round(argFirstNumber, 2), Math.Round(argSecondNumber, 2), Math.Round(result, 2)));
                     Console.WriteLine(strbuild.ToString());
                     break;
-                case "root":
-                case "square root":
-                case "v":
+                case "root1":
+                case "square root1":
+                case "sqr1":
                     result = Math.Sqrt(argFirstNumber);
                     strbuild.Append(String.Format("The square root of the first value you entered, {0}, is equal to {1}", Math.Round(argFirstNumber, 2), Math.Round(result, 2)));
                     Console.WriteLine(strbuild.ToString());
                     break;
                 case "power":
-                case "^":
+                case "pow":
                     result = Math.Pow(argFirstNumber,argSecondNumber);
                     strbuild.Append(String.Format("The value {0} to the power of the value {1} is equal to {2}", Math.Round(argFirstNumber, 2), Math.Round(argSecondNumber, 2), Math.Round(result, 2)));
                     Console.WriteLine(strbuild.ToString());
+                    break;
+                case "&":
+                case "and":
+                    result = (int)argFirstNumber & (int)argSecondNumber;
+                    strbuild.Append(String.Format("The bitwise AND value of {0} and {1} is equal to {2}", (int)argFirstNumber, (int)argSecondNumber, result));
+                    Console.WriteLine(strbuild.ToString());
+                    break;
+                case "|":
+                case "or":
+                    result = (int)argFirstNumber | (int)argSecondNumber;
+                    strbuild.Append(String.Format("The bitwise OR value of {0} and {1} is equal to {2}", (int)argFirstNumber, (int)argSecondNumber, result));
+                    Console.WriteLine(strbuild.ToString());
+                    break;
+                case "^":
+                case "xor":
+                    result = (int)argFirstNumber ^ (int)argSecondNumber;
+                    strbuild.Append(String.Format("The bitwise XOR value of {0} and {1} is equal to {2}", (int)argFirstNumber, (int)argSecondNumber, result));
+                    Console.WriteLine(strbuild.ToString());
+                    break;
+                case ">>":
+                case "rightshift":
+                    result = (int)argFirstNumber >> (int)argSecondNumber;
+                    if ((int)argSecondNumber == 1)
+                    {
+                        strbuild.Append(String.Format("The value {0} shifted {1} unit to the right is equal to {2}", (int)argFirstNumber, (int)argSecondNumber, result));
+                        Console.WriteLine(strbuild.ToString());
+                    }
+                    else
+                    {
+                        strbuild.Append(String.Format("The value {0} shifted {1} units to the right is equal to {2}", (int)argFirstNumber, (int)argSecondNumber, result));
+                        Console.WriteLine(strbuild.ToString());
+                    }
+                    break;
+                case "<<":
+                case "leftshift":
+                    result = (int)argFirstNumber << (int)argSecondNumber;
+                    if ((int)argSecondNumber == 1)
+                    {
+                        strbuild.Append(String.Format("The value {0} shifted {1} unit to the left is equal to {2}", (int)argFirstNumber, (int)argSecondNumber, result));
+                        Console.WriteLine(strbuild.ToString());
+                    }
+                    else
+                    {
+                        strbuild.Append(String.Format("The value {0} shifted {1} units to the left is equal to {2}", (int)argFirstNumber, (int)argSecondNumber, result));
+                        Console.WriteLine(strbuild.ToString());
+                    }
                     break;
                 default:
                     strbuild.Append("+-------------------+" +
@@ -59,8 +105,15 @@ namespace SimpleCalculator
                                     "\n2.Subtraction: \"minus\",\"subtract\",\"-\"" +
                                     "\n3.Multiplication: \"multiply\",\"times\",\"*\"" +
                                     "\n4.Division: \"divide\",\"\\\"" + 
-                                    "\n5.Square Root:\"root\",\"square root\""+ 
-                                    "\n6.Power:\"power\",\"^\"");
+                                    "\n5.Square Root(first value):\"root1\",\"square root1\",\"sqr1\""+
+                                    "\n6.Square Root(second value):\"root2\",\"square root2\",\"sqr2\"" +
+                                    "\n7.Power:\"power\",\"^\""+
+                                    "\n8(*).Bitwise AND: \"and\",\"&\"" +
+                                    "\n9(*).Bitwise OR: \"or\",\"|\"" +
+                                    "\n10(*).Bitwise XOR: \"xor\",\"^\"" +
+                                    "\n11(*).Right shift: \"rightshift\",\">>\"" +
+                                    "\n12(*).Left shift: \"leftshift\",\"<<\"" +
+                                    "\n(*) Expect a floating point value to be truncated" + "\n");
                     throw new InvalidOperationException(strbuild.ToString());
             }
             return result;
--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -12,8 +12,20 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            
+
+            //Class to convert user input
+            InputConverter inputConverter = new InputConverter();
+
+            //Class to perform actual calculations
+            CalculatorEngine calculatorEngine = new CalculatorEngine();
+
             string startOver = "y";
+            string operation = "";
+            double firstNumber = 0.0;
+            double secondNumber = 0.0;
+            double result = 0.0;
+
+
             Console.WriteLine("   _____  _____  _____          _      _____ ");
             Console.WriteLine("  / ____|/ ____|/ ____|   /\\   | |    / ____|");
             Console.WriteLine(" | |    | (___ | |       /  \\  | |   | |     ");
@@ -24,40 +36,53 @@ namespace SimpleCalculator
             Console.WriteLine("Made by Manh-Anh Tuan Nguyen & Olivier Lemire, (c) 2020.");
             while (startOver.Equals("y"))
             {
-                bool error = true;
-                while (error)
+                bool error_num1 = true;
+                bool error_num2 = true;
+                bool error_opt = true;
+                while (error_num1)
                 {
                     try
                     {
-                        //Class to convert user input
-                        InputConverter inputConverter = new InputConverter();
 
-                        //Class to perform actual calculations
-                        CalculatorEngine calculatorEngine = new CalculatorEngine();
                         Console.WriteLine("Please enter a number(must be a double):");
-                        double firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-
-                        Console.WriteLine("Great! Now enter another number(must be a double):");
-                        double secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
-
-                        Console.WriteLine("What should I do with the 2 values?");
-                        string operation = Console.ReadLine();
-
-                        double result = calculatorEngine.Calculate(operation.ToLower(), firstNumber, secondNumber);
-
-
-                        //Console.WriteLine("{0} result");
-                        error = false;
-
+                        firstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                        error_num1 = false;
                     }
-                    catch (Exception ex)
+                    catch (Exception e)
                     {
-                        Console.WriteLine(ex.Message);
-                        error = true;
+                        Console.WriteLine(e.Message);
+                        error_num1 = true;
                     }
-
                 }
-                Console.WriteLine("Would you like to make another calculation?(Press y to start over)");
+                while(error_num2)
+                {
+                    try
+                    {
+                        Console.WriteLine("Now enter another number(must be a double):");
+                        secondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                        error_num2 = false;
+                    }catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        error_num2 = true;
+                    }
+                   
+                }
+                while (error_opt)
+                {
+                    try
+                    {
+                        Console.WriteLine("What should I do with the 2 values?");
+                        operation = Console.ReadLine();
+                        result = calculatorEngine.Calculate(operation.ToLower(), firstNumber, secondNumber);
+                        error_opt = false;
+                    }catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        error_opt = true;
+                    }
+                }      
+                Console.WriteLine("Would you like to do another calculation?(Press y to start over)");
                 startOver = Console.ReadLine();
             }
 
